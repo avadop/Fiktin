e3fdb2f5dc2bd9108f48008a9c57626daec8402d
Andrea Martínez <deitamf@hotmail.com>
2020-02-19 23:25:05 +0100

    Added cool but not working header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,45 @@
 <template>
   <div id="app">
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="/home">Fiktin</b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="/books">Libros</b-nav-item>
+            <b-nav-item href="/libraries">Bibliotecas</b-nav-item>
+            <label class="my-2">Bonito, pero no funciona (por eso de que se cierra la sesión al recargar la página)</label>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-form>
+              <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+              <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+            </b-nav-form>
+
+            <b-nav-item-dropdown text="Lang" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template v-slot:button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="/user">Usuario</b-dropdown-item>
+              <b-dropdown-item href="/" @click.native="confirmLogOut">Cerrar Sesión</b-dropdown-item>
+              <!-- <b-dropdown-item v-else="loggedIn" href="/home" @click.native="confirmLogOut">Iniciar Sesión</b-dropdown-item> -->
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
     <div id="nav" v-if="loggedIn">
       <router-link to="/home">Home</router-link> |
       <router-link to="/books">Libros </router-link> |
