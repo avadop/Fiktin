307141db397a4ddf7f2705b05da1ccf46aa91848
Andrea Martínez <deitamf@hotmail.com>
2020-02-19 23:00:32 +0100

    Home: working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,43 @@
 <template>
-<h1>HOME</h1>
-
-<div class="booksBox" v-for="(book, idx) in books" :key="idx">
-  <br>
-  <div>
-    <!-- Titulo -->
-    <span>
-      Título: {{ book.title }}
-    </span>
+<div class="home">
+  <h1>HOME</h1>
+  <div class="booksBox" v-for="(book, idx) in books" :key="idx">
     <br>
-
-    <!-- Autor -->
-    <h6 v-if="book.author == 'Nombre'">{{ book.name }} </h6>
-    <h6 v-else>{{ book.nick }}</h6>
-    <br>
-
-    <!-- Descripcion -->
-    <span v-if="book.description">
-      Descripción: {{ book.description }}
+    <div>
+      <!-- Titulo -->
+      <span>
+        Título: {{ book.title }}
+      </span>
       <br>
-    </span>
 
-    <!-- Portada -->
-    <span v-if="book.cover != null">
-      Portada:
+      <!-- Autor -->
+      <h6 v-if="book.author == 'Nombre'">{{ book.name }} </h6>
+      <h6 v-else>{{ book.nick }}</h6>
       <br>
-      <img width="320" :src="book.cover">
-      <br>
-    </span>
 
-    <!-- Etiquetas -->
-    <span class="etiquetas" v-if="book.tags.length > 0">
-      Etiquetas:
-      <div class="etiqueta">
-        <li v-for="(tag, idt) in book.tags" :key="idt">{{ tag }}</li>
-      </div>
-      <br>
-    </span>
+      <!-- Descripcion -->
+      <span v-if="book.description">
+        Descripción: {{ book.description }}
+        <br>
+      </span>
+
+      <!-- Portada -->
+      <span v-if="book.cover != null">
+        Portada:
+        <br>
+        <img width="320" :src="book.cover">
+        <br>
+      </span>
+
+      <!-- Etiquetas -->
+      <span class="etiquetas" v-if="book.tags.length > 0">
+        Etiquetas:
+        <div class="etiqueta">
+          <li v-for="(tag, idt) in book.tags" :key="idt">{{ tag }}</li>
+        </div>
+        <br>
+      </span>
+    </div>
   </div>
 </div>
 </template>
@@ -48,7 +49,7 @@ export default {
   name: 'homeBooksList',
   data () {
     return {
-      books: [],
+      books: []
     }
   },
   mounted () {
@@ -60,22 +61,22 @@ export default {
       booksCollection.get().then(snapshot => {
         snapshot.forEach(doc1 => {
           if (doc1.data().published) {
-				    userCollection.doc(doc1.data().user_id).get().then(doc2 => {
-	            this.books.push({
-	              author: doc1.data().author,
-	              title: doc1.data().title,
-	              description: doc1.data().description,
-	              cover: doc1.data().cover,
-	              tags: doc1.data().tags,
-	              ID: doc1.id
-					      nick = doc2.nick
-					      name = doc2.name
-	            })
-				    })
+            userCollection.doc(doc1.data().user_id).get().then(doc2 => {
+              this.books.push({
+                author: doc1.data().author,
+                title: doc1.data().title,
+                description: doc1.data().description,
+                cover: doc1.data().cover,
+                tags: doc1.data().tags,
+                ID: doc1.data().user_id,
+                nick: doc2.data().nick,
+                name: doc2.data().name
+              })
+            })
           }
         })
       })
-    },
+    }
   }
 }
 </script>
