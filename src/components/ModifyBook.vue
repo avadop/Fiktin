69ca94c5e386b2fb85f57e30546aaef9a52ef917
Andrea Martínez <deitamf@hotmail.com>
2020-02-19 23:23:00 +0100

    Improved modify book style

--- src/components/ModifyBook.vue
+++ src/components/ModifyBook.vue
@@ -1,89 +1,116 @@
 <template>
   <div class="modify" >
-    <!-- titulo -->
-    <span>
-      Título: <input v-model="title" type="text" placeholder="Título del libro">
-    </span>
-    <br>
+    <h4>Editar libro</h4>
+    <b-container class="row">
+      <b-container class="col">
+        <!-- titulo -->
+        <b-container fluid class="row">
+          <b-row class="my-1">
+            <b-form-group>
+              <label>Título</label>
+              <b-form-input type="text" v-model="title" placeholder="Título del libro"></b-form-input>
+            </b-form-group>
+          </b-row>
+        </b-container>
 
-    <!-- autor -->
-    <span>
-      Autor:
-      <div class="etiquetas">
-        <input type="radio" id="nombre" value="Nombre" v-model="author">
-        <label for="one"> Nombre de usuario</label>
-        <br>
-        <input type="radio" id="nick" value="Nick" v-model="author">
-        <label for="two"> Nickname</label>
-      </div>
-    </span>
-    <br>
+        <!-- autor -->
+        <b-container fluid class="row">
+          <b-row class="my-1">
+            <b-form-group>
+              <label>Autor</label>
+              <b-form-group>
+                <b-form-radio v-model="author" value="Nombre"> Nombre de usuario</b-form-radio>
+                <b-form-radio v-model="author" value="Nick"> Nickname</b-form-radio>
+              </b-form-group>
+            </b-form-group>
+          </b-row>
+        </b-container>
+      </b-container>
 
-    <!-- descripcion -->
-    <span>
-      Descripción:
-      <br>
-      <textarea class="model" v-model="description" placeholder="Descripción"/>
-      <br>
-    </span>
+      <!-- portada -->
+      <b-container fluid class="col">
+        <b-row class="my-1">
+          <b-container sm="3">
+            <label>Portada</label>
+            <b-form-file
+              @change="onFileSelected"
+              class="my-2"
+              placeholder="Selecciona una imagen o arrastrala aquí..."
+              drop-placeholder="Arrastra aquí la imagen..."
+              accept="image/*"
+            ></b-form-file>
+          </b-container>
+        </b-row>
+        <b-row class="my-1">
+          <b-col sm="9">
+            <b-img :src="this.cover" fluid width="250%" alt="No has subido ninguna imagen"></b-img>
+            <b-button v-if="this.cover != null" class="my-2" variant="danger" @click="removeImg">Eliminar</b-button>
+          </b-col>
+        </b-row>
+      </b-container>
+    </b-container>
 
-    <!-- portada -->
-    <span>
-      Portada:
-      <br>
-      <img width="320" :src="this.cover">
-      <b-button v-if="this.cover != null" variant="danger" @click="removeImg">Eliminar</b-button>
-      <br>
-    </span>
+    <b-container class="row">
+      <!-- etiquetas -->
+      <b-container fluid class="col">
+        <b-row class="my-1">
+          <b-form-group>
+            <label>Etiquetas</label>
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="accion_aventura_modify_id" value="AccionAventura" v-model="tags">
+            <label class="custom-control-label" for="accion_aventura_modify_id"> Acción y aventura</label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="ciencia_ficcion_modify_id" value="CienciaFiccion" v-model="tags">
+            <label class="custom-control-label" for="ciencia_ficcion_modify_id"> Ciencia ficción</label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="comedia_modify_id" value="Comedia" v-model="tags">
+            <label class="custom-control-label" for="comedia_modify_id"> Comedia</label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="crimen_modify_id" value="Crimen" v-model="tags">
+            <label class="custom-control-label" for="crimen_modify_id"> Crimen</label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="drama_modify_id" value="Drama" v-model="tags">
+            <label class="custom-control-label" for="drama_modify_id"> Drama</label>
+          </div>
+          <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="fantasia_modify_id" value="Fantasia" v-model="tags">
+            <label class="custom-control-label" for="fantasia_modify_id"> Fantasia</label>
+          </div>
+          </b-form-group>
+        </b-row>
+      </b-container>
 
-    <br>
-    <input type="file" name="modify_cover" @change="onFileSelected" accept="image/*">
-    <br>
+      <!-- descripcion -->
+      <b-container fluid class="col">
+        <b-row class="my-1">
+          <b-container sm="3">
+            <label>Descripción</label>
+            <b-form-textarea
+              v-model="description"
+              placeholder="Descripción"
+              rows="3"
+              max-rows="6"
+            ></b-form-textarea>
+          </b-container>
+        </b-row>
+      </b-container>
+    </b-container>
 
-    <!-- etiquetas -->
-    <span>
-      Etiquetas:
-      <div class="etiquetas ">
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="accion_aventura_modify_id" value="AccionAventura" v-model="tags">
-          <label class="custom-control-label" for="accion_aventura_modify_id"> Acción y aventura</label>
-        </div>
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="ciencia_ficcion_modify_id" value="CienciaFiccion" v-model="tags">
-          <label class="custom-control-label" for="ciencia_ficcion_modify_id"> Ciencia ficción</label>
-        </div>
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="comedia_modify_id" value="Comedia" v-model="tags">
-          <label class="custom-control-label" for="comedia_modify_id"> Comedia</label>
-        </div>
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="crimen_modify_id" value="Crimen" v-model="tags">
-          <label class="custom-control-label" for="crimen_modify_id"> Crimen</label>
-        </div>
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="drama_modify_id" value="Drama" v-model="tags">
-          <label class="custom-control-label" for="drama_modify_id"> Drama</label>
-        </div>
-        <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="fantasia_modify_id" value="Fantasia" v-model="tags">
-          <label class="custom-control-label" for="fantasia_modify_id"> Fantasia</label>
-        </div>
-      </div>
-      <br>
-    </span>
-
-    <!-- publicado -->
-    <span>
-      <div class="custom-control custom-checkbox">
+    <!-- botones -->
+    <div class="d-flex justify-content-end">
+      <b-button variant="danger" class="mr-auto" @click="deleteButton">Eliminar</b-button>
+      <!-- publicado -->
+      <div class="custom-control custom-checkbox my-4">
         <input type="checkbox" class="custom-control-input" id="published_modify_id" v-model="published">
         <label class="custom-control-label" for="published_modify_id"> Publicar</label>
       </div>
-    </span>
-
-    <!-- botones -->
-    <b-button variant="danger" @click="deleteButton">Eliminar</b-button>
-    <b-button variant="secondary" @click="cancelButton">Descartar</b-button>
-    <b-button variant="success" @click="saveButton" :disabled="this.UploadValue != 0 && this.UploadValue != 100">Guardar</b-button>
+      <b-button variant="secondary" @click="cancelButton">Descartar</b-button>
+      <b-button variant="success" @click="saveButton" :disabled="this.UploadValue != 0 && this.UploadValue != 100">Guardar</b-button>
+    </div>
   </div>
 </template>
 
@@ -113,7 +140,7 @@ export default {
       this.cover = null
     },
     onFileSelected (event) {
-      this.selectedFile = event.target.files[0] // coge el nombre de la primera imagen subida
+      this.selectedFile = event.target.files[0]
       this.onUpload()
     },
     onUpload () {
--- src/router/index.js
+++ src/router/index.js
@@ -10,10 +10,12 @@ import ViewLibrary from '../views/ViewLibrary.vue'
 
 // bootstrap
 import BootstrapVue from 'bootstrap-vue'
+// import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
+// Vue.use(BootstrapVueIcons)
 
 Vue.use(VueRouter)
 
