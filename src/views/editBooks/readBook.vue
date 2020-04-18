89ff8cc0095cafc5fb674d7dd0a6b64e5c81af0e
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2020-04-18 14:12:09 +0200

    lectura + adivinanza

--- src/views/editBooks/readBook.vue
+++ src/views/editBooks/readBook.vue
@@ -5,10 +5,21 @@
       <button class="buttonEdit" v-if="isBookOfLoggedUser() && book.published===false" @click="goEdit()">Editar</button>
       <button class="buttonEditDisabled" v-else-if="isBookOfLoggedUser()" :disabled="book.published===true">Editar</button>
       <span style="color: red; padding-left: 10px;" v-if="isBookOfLoggedUser() && book.published===true">No se puede editar un libro si este se encuentra publicado</span>
+      <b-row>
+        <b-col cols="3"><h5>Sección: {{ sectionName }}</h5></b-col>
+        <b-col cols="3"><b-form-select size="sm" v-model="currentSectionID" :options="sectionsData" @change="loadSection(currentSectionID)"></b-form-select></b-col>
+      </b-row>
     </div>
     <div class="readBook">
       <div v-for="(text, index) in sectionGadgets" :key="index">
-        <span v-html="text.htmlText"/>
+        <span v-if="basicGadget(text)" v-html="text.htmlText"/>
+        <RiddleReading v-if="text.componentName === 'riddle'"
+         :numberOfTriesAux="text.numberOfTries"
+         :riddleText="text.riddleText"
+         :answerText="text.answerText"
+         :wrongSection="text.wrongSection"
+         :rightSection="text.rightSection"
+         @answered="loadSection"/>
       </div>
     </div>
   </div>
@@ -17,9 +28,13 @@
 <script>
 import { librariesCollection, sectionsCollection } from '@/firebase.js'
 import { store } from '../../store/index.js'
+import RiddleReading from '@/components/readingGadgets/RiddleReading.vue'
 
 export default {
   name: 'readBook',
+  components: {
+    RiddleReading
+  },
   props: {
     book: Object,
     bookID: String
@@ -27,19 +42,34 @@ export default {
   data () {
     return {
       searchNick: store.state.userNick.concat('_historial').toLowerCase(),
-      currentSection: '',
-      sectionGadgets: []
+      currentSectionID: '',
+      sectionGadgets: [],
+      sectionsData: [],
+      sectionName: ''
     }
   },
   mounted () {
     if (this.bookID !== undefined) this.addBookToLibrary()
-    this.loadSection(0)
+    this.currentSectionID = this.book.sections[0]
+    this.loadSection(this.book.sections[0])
+    this.loadBook()
   },
   methods: {
-    async loadSection (sectionIndex) {
-      this.currentSection = sectionIndex
-      await sectionsCollection.doc(this.book.sections[sectionIndex]).get().then(doc => {
+    async loadBook () {
+      this.sectionsData = []
+      for (var i = 0; i < this.book.sections.length; ++i) {
+        await sectionsCollection.doc(this.book.sections[i]).get().then(doc => {
+          if (doc.exists) {
+            this.sectionsData.push({ value: doc.id, text: doc.data().name })
+          }
+        })
+      }
+    },
+    async loadSection (sectionID) {
+      this.currentSectionID = sectionID
+      await sectionsCollection.doc(sectionID).get().then(doc => {
         this.sectionGadgets = doc.data().gadgets
+        this.sectionName = doc.data().name
       })
     },
     addBookToLibrary: async function () {
@@ -54,13 +84,13 @@ export default {
         array_keys: a
       })
     },
+    // Los que su html se pueda poner directamente se añaden aqui
+    basicGadget (value) {
+      return value.componentName === 'Título' || value.componentName === 'Texto normal' || value.componentName === 'Multimedia'
+    },
     isBookOfLoggedUser () {
       return this.book.userID === store.state.userID
     },
-    // saveHTML (plainText, htmlText, index) {
-    //   this.data[index].plainText = plainText
-    //   this.data[index].htmlText = htmlText
-    // },
     goEdit () {
       this.$router.replace({ name: 'editBook', params: { book: this.book, bookID: this.bookID } })
     },
