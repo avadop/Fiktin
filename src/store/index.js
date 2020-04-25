6f52e280fad06ca69f50f29a2f8250b47bdcbd59
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2020-04-25 13:43:03 +0200

    Se queda log in

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
   state: {
     userID: '',
-    userNick: ''
+    userNick: '',
+    loggedIn: false
   },
+  plugins: [createPersistedState()],
   mutations: {
     logIn (state, id) {
       state.userID = id
+      state.loggedIn = true
     },
     logOut (state) {
       state.userID = ''
+      state.loggedIn = false
     },
     nickChange (state, nick) {
       state.userNick = nick
