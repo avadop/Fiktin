01af16ddbdf9fb46efd75807a85d67ade05c4f70
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2020-04-30 18:12:37 +0200

    colores base  cambiados

--- src/components/readingGadgets/MemoryCardsReading.vue
+++ src/components/readingGadgets/MemoryCardsReading.vue
@@ -5,7 +5,7 @@
     <b-row style="margin-left: 15px;">
       <div v-for="(card, index) in cards" :key="index">
           <b-button v-if="card.flipped === false" :disabled="numberOfCardsFlipped === 2 || (numberOfMoves === maxNumberOfMoves)" style="height: 75px; width: 75px; margin: 10px; margin-top: 0px;" @click="flipCard(index)"></b-button>
-          <b-button v-else :variant="card.image" style="height: 75px; width: 75px; margin: 10px; margin-top: 0px;"></b-button>
+          <b-button v-else class="memory-card" :style="card.color"></b-button>
       </div>
     </b-row>
     <p v-if="numberOfMoves === maxNumberOfMoves && this.numberOfPairsMissing > 0" style="color: red;">Te has quedado sin movimientos :(</p>
@@ -27,18 +27,19 @@ export default {
   },
   data () {
     return {
-      cardTypes: [{ image: 'primary' },
-        { image: 'danger' },
-        { image: 'success' },
-        { image: 'warning' },
-        { image: 'dark' },
-        { image: 'info' },
-        { image: 'light' },
-        { image: 'outline-success' },
-        { image: 'outline-warning' },
-        { image: 'outline-danger' },
-        { image: 'outline-primary' },
-        { image: 'outline-dark' }],
+      cardTypes: [{ color: '#4CAF50' }, // verde
+        { color: '#008CBA' }, // azul
+        { color: '#FF9713' }, // naranja
+        { color: '#EE2A1C' }, // rojo
+        { color: '#A97FCB' }, // lila
+        { color: '#F12CDF' }, // fuxia
+        { color: '#18FCC5' }, // aguamarina
+        { color: '#5E22F1' }, // purpura
+        { color: '#F0A899' }, // naranjita
+        { color: '#074F1D' }, // verde oscuro
+        { color: '#5F380A' }, // marron
+        { color: '#DEF122' } // verde lima
+      ],
       cards: [],
       numberOfCardsFlipped: 0,
       cardClickedBefore: '',
@@ -52,13 +53,13 @@ export default {
   },
   methods: {
     shuffleCards () {
-      for (var i = 0; i < this.numberOfPairs * 2; ++i) this.cards.push({ image: '', flipped: Boolean })
+      for (var i = 0; i < this.numberOfPairs * 2; ++i) this.cards.push({ color: '', flipped: Boolean })
       var value = 1
       var j = 0
       while (j < this.numberOfPairs * 2) {
         var index = Math.floor(Math.random() * (this.numberOfPairs * 2))
-        if (this.cards[index].image === '') {
-          this.cards[index].image = this.cardTypes[value - 1].image
+        if (this.cards[index].color === '') {
+          this.cards[index].color = 'background-color: ' + this.cardTypes[value - 1].color + ';'
           this.cards[index].flipped = false
           j++
           if (j % 2 === 0) value++
@@ -71,7 +72,7 @@ export default {
       this.numberOfMoves++
 
       if (this.numberOfCardsFlipped === 1) this.cardClickedBefore = index
-      if (this.numberOfCardsFlipped === 2 && (this.cards[index].image === this.cards[this.cardClickedBefore].image)) {
+      if (this.numberOfCardsFlipped === 2 && (this.cards[index].color === this.cards[this.cardClickedBefore].color)) {
         this.numberOfCardsFlipped = 0
         this.numberOfPairsMissing--
         if (this.numberOfPairsMissing === 0) this.solved = true
@@ -84,7 +85,7 @@ export default {
       if (this.numberOfMoves === this.maxNumberOfMoves && this.numberOfPairsMissing > 0) {
         setTimeout(() => { this.checkAnswer() }, 700)
       }
-      if (this.numberOfCardsFlipped === 2 && (this.cards[index].image !== this.cards[this.cardClickedBefore].image)) {
+      if (this.numberOfCardsFlipped === 2 && (this.cards[index].color !== this.cards[this.cardClickedBefore].color)) {
         setTimeout(() => { this.refresh(index) }, 1000)
       }
     },
@@ -104,3 +105,13 @@ export default {
   }
 }
 </script>
+
+<style>
+.memory-card {
+  height: 75px;
+  width: 75px;
+  margin: 10px;
+  margin-top: 0px;
+  border: none;
+}
+</style>
