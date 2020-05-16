a7aed091e8fcedc00d5cf3ba4b386fa6a6a13ca8
Andrea Martínez <deitamf@hotmail.com>
2020-05-16 14:09:50 +0200

    Changed icons when user is not logged

--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,7 @@
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-item @click="needLogIn('/')"><div class=" h4 nav-ref"><b-icon icon="person"></b-icon></div></b-nav-item>
-            <b-nav-item ><div @click="confirmLogOut" class="h4 nav-ref"><b-icon icon="power"></b-icon></div></b-nav-item>
+            <b-nav-item @click="needLogIn('/')"><div class=" h4 nav-ref"><b-icon icon="box-arrow-in-right"></b-icon></div></b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
