801164a71726e498b8a7c62a8361f1fed31d7c37
Andrea del Vado Puell <andrea.vado.puell@gmail.com>
2020-05-19 18:28:43 +0200

    logOut - icon try 2

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,8 @@
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
-            <b-nav-item><router-link to="/user" class="h4 nav-ref" style="font-size: 16px;"><b-icon icon="person"></b-icon></router-link></b-nav-item>
-            <b-nav-item><div @click="confirmLogOut" class="h4 nav-ref" style="font-size: 16px;"><b-icon icon="power"></b-icon></div></b-nav-item>
+            <b-nav-item style="font-size: 16px;"><router-link to="/user" class="h4 nav-ref" ><b-icon icon="person"></b-icon></router-link></b-nav-item>
+            <b-nav-item style="font-size: 16px;"><div @click="confirmLogOut" class="h4 nav-ref"><b-icon icon="power"></b-icon></div></b-nav-item>
           </b-navbar-nav>
         </b-collapse>
 
@@ -42,13 +42,13 @@
           <h3>Cerrar Sesión</h3>
           <p>¿Está seguro que desea cerrar sesión?</p>
         </div>
-        <b-button class="mt-3" variant="outline-danger" block @click="logOut">Si</b-button>
-        <b-button class="mt-2" variant="primary" block @click="modalLogOut = false">Cancelar</b-button>
+        <b-button class="mt-3" variant="outline-danger" block @click="logOut" style="font-size: 16px;">Si</b-button>
+        <b-button class="mt-2" variant="primary" block @click="modalLogOut = false" style="font-size: 16px;">Cancelar</b-button>
     </b-modal>
 
     <b-modal v-model="modalLogIn" hide-footer hide-header no-close-on-backdrop centered>
       <start :urlLogIn="urlLogIn" @loggedIn="logIn"/>
-      <b-button class="mt-2" variant="outline-secondary" size="sm" block @click="modalLogIn = false">Cancelar</b-button>
+      <b-button class="mt-2" variant="outline-secondary" size="sm"  block @click="modalLogIn = false">Cancelar</b-button>
     </b-modal>
   </div>
 </template>
